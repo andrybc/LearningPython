fc67043b0afb3ce74d1bae12e559f7f92346a75e
andrybc <canelbandry@gmail.com>
2022-07-24 20:43:49 -0400

    gives error if i put string before input command unlike for int

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-print('Andry Canel')
-print('o----')
-print(' ||||')
-print('L' * 20)
-price = 100
-print(price)
+# print('Andry Canel')
+# print('o----')
+# print(' ||||')
+# print('L' * 20)
+# price = 100
+# print(price)
+print("Welcome to Florida Hospital! Please sign in the patient on our clipboard")
+name = input("Name: ")
+DOB = input("Birthdate: ")
+phoneNum = int(input("Cell: "))
+
+
